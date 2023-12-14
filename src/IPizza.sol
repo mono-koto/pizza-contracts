1bf6605bd86e1c3581c9592f777cf6da3d1949b7
Brent Fitzgerald <brent@mono-koto.com>
2023-12-14 14:29:40 -0800

    remove oz contracts-upgradeable, upgrade oz, update tests/code

--- src/IPizza.sol
+++ src/IPizza.sol
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.23;
 
-import {PaymentSplitterUpgradeable} from "openzeppelin-contracts-upgradeable/finance/PaymentSplitterUpgradeable.sol";
-import {Initializable} from "openzeppelin-contracts-upgradeable/proxy/utils/Initializable.sol";
+import {Initializable} from "openzeppelin-contracts/proxy/utils/Initializable.sol";
 
 interface IPizza {
     function initialize(address[] memory _payees, uint256[] memory _shares) external;
--- test/PizzaFactory.t.sol
+++ test/PizzaFactory.t.sol
@@ -1,15 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity 0.8.23;
 
 import {Test, console2} from "forge-std/Test.sol";
 import {PizzaFactory, PizzaCreated} from "../src/PizzaFactory.sol";
 import {Pizza} from "../src/Pizza.sol";
 import {Address} from "openzeppelin-contracts/utils/Address.sol";
 import {IERC20} from "openzeppelin-contracts/token/ERC20/IERC20.sol";
+import {Clones} from "openzeppelin-contracts/proxy/Clones.sol";
 
 event PaymentReceived(address from, uint256 amount);
 
-contract CounterTest is Test {
+contract PizzaFactoryTest is Test {
     PizzaFactory public f;
     address[] payees;
     uint256[] shares;
@@ -47,7 +48,7 @@ contract CounterTest is Test {
 
     function test_createDeterministicOnce(uint256 nonce) public {
         Pizza(payable(address(f.createDeterministic(payees, shares, nonce))));
-        vm.expectRevert("ERC1167: create2 failed");
+        vm.expectRevert(abi.encodeWithSelector(Clones.ERC1167FailedCreateClone.selector));
         f.createDeterministic(payees, shares, nonce);
     }
 
